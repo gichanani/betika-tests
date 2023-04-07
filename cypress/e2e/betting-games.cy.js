6a2d99f255a9361262e70f1b79373586e0da6f01
gichanani <nicholasgichana@gmail.com>
2023-04-07 17:44:14 +0300

    used random games command

--- cypress/e2e/betting-games.cy.js
+++ cypress/e2e/betting-games.cy.js
@@ -7,25 +7,6 @@ describe('Betting games', () => {
   })
 
   it.only('Select random games', () => {
-
-        // Loop through 10 games
-    for (let i = 0; i < 10; i++) {
-      // Find the game container
-      cy.get('.prebet-match__odd-market__container').eq(i).within(() => {
-
-        cy.wait(500).get('.prebet-match__odd').then(buttons => {
-          // Select a random button
-          const randomIndex = Math.floor(Math.random() * buttons.length);
-          const randomButton = buttons.eq(randomIndex);
-
-          // Click the button and confirm the selection
-          randomButton.trigger('click')
-        });
-
-  });
-}
-
- 
-    
+    cy.randomgames()
   })
 })
