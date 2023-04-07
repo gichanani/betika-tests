573dc4d57a640e81c5f08d83c5b4b57e77e10daa
gichanani <nicholasgichana@gmail.com>
2023-04-07 17:43:29 +0300

    login test cases

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,6 +1,52 @@
+import loginPage from '../support/pom/loginPage'
+const account = require('../fixtures/user.json')
+
 describe('Login', () => {
-  it('Navigate to login page', () => {
+  beforeEach( () => {
     cy.visit('/login')
-    
   })
+
+  it('Navigate to login page', () => {
+    cy.location('pathname').should('eq','/en-ke/login')
+  })
+
+  it('Validate required phone number field', () => {
+    loginPage.elements.loginBtn()
+    loginPage.elements.error().should('contain','Please enter a phone number')
+  })
+
+  it.only('Validate required password field', () => {
+    loginPage.elements.username().type(account.testaccount)
+    loginPage.elements.loginBtn()
+    loginPage.elements.error().should('contain','Please enter a password')
+  })
+
+  it('Login with valid credentials', () => {
+    loginPage.elements.username().type(account.testaccount)
+    loginPage.elements.password().type(account.password)
+    loginPage.elements.loginBtn()
+    cy.location('pathname').should('contain','en-ke')
+  })
+
+  it('Login with wrong password', () => {
+    loginPage.elements.username().type(account.testaccount)
+    loginPage.elements.password().type(account.invalid_password)
+    loginPage.elements.loginBtn()
+    loginPage.elements.notification().should('contain','The mobile and password provided do not match')
+  })
+
+  it('Login with wrong phone number', () => {
+    loginPage.elements.username().type(account.wrongaccount)
+    loginPage.elements.password().type(account.invalid_password)
+    loginPage.elements.loginBtn()
+    loginPage.elements.notification().should('contain','The mobile and password provided do not match')
+  })
+
+  it('Login with invalid phone number', () => {
+    loginPage.elements.username().type(account.invalid_testaccount)
+    loginPage.elements.password().type(account.invalid_password)
+    loginPage.elements.loginBtn()
+    loginPage.elements.error().should('contain','This phone number is invalid')
+  })
+
 })
