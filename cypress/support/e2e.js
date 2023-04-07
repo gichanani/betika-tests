31f125856642984ce574a7964ca40121ad3e77a2
gichanani <nicholasgichana@gmail.com>
2023-04-07 20:06:28 +0300

    installed allure reporter

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -12,11 +12,11 @@
 // You can read more here:
 // https://on.cypress.io/configuration
 // ***********************************************************
-
+/// <reference types="Cypress" />
 // Import commands.js using ES2015 syntax:
 import './commands'
 
 require('cypress-xpath');
-
+import "@shelex/cypress-allure-plugin"
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
